7294440a916c0525da0e3694b5bc2c7795e3cc0c
Luzzi Charly <luzzi.charly@gmail.com>
2020-06-22 13:25:56 +0200

    Searchbar

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,25 @@
             <Menu/>
             <div class="ion-page" id="main-content">
                 <ion-header>
-                    <ion-toolbar>
+                    <ion-toolbar color="tertiary">
                         <ion-buttons slot="start">
                             <ion-menu-button></ion-menu-button>
                         </ion-buttons>
                         <ion-title>SneakerX</ion-title>
+                        <ion-buttons slot="primary">
+                            <ion-button @click="displaySearchBar = !displaySearchBar">
+                                <ion-icon slot="icon-only" name="search"></ion-icon>
+                            </ion-button>
+                        </ion-buttons>
                     </ion-toolbar>
+                    <transition name="slideUp">
+                        <ion-searchbar animated class="searchbar"
+                                       v-show="displaySearchBar">
+                        </ion-searchbar>
+                    </transition>
                 </ion-header>
 
-                <ion-content>
+                <ion-content class="main-container">
                     <ion-vue-router/>
                 </ion-content>
 
@@ -37,6 +47,11 @@
         components: {
           Menu
         },
+        data () {
+            return {
+                displaySearchBar: false
+            }
+        },
         methods: {
             goToWebSite() {
                 window.open(`${process.env.VUE_APP_WEB_URL}/`, '_blank');
@@ -47,5 +62,21 @@
 
 
 <style lang="scss">
+    ion-searchbar {
+        padding: 6px 10px !important;
+    }
 
+    .slideUp-enter-active, .slideUp-leave-active {
+        transition: all .5s;
+    }
+    .slideUp-enter, .slideUp-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+        transform: translateY(-50px);
+    }
+    .main-container {
+        position: absolute;
+    }
+    .searchbar {
+        background-color: white;
+    }
 </style>
